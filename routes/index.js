83086cf7daf6747c65aeb2cc403814a85f8a61dd
bxw201 <bxw201@nyu.edu>
2021-06-28 16:26:19 -0700

    Moving body-parser back to app

--- routes/index.js
+++ routes/index.js
@@ -2,30 +2,17 @@ var express = require('express');
 const http = require('http');
 var router = express.Router();
 
-var bodyParser = require('body-parser');
-var jsonParser = bodyParser.json();
-
-// function requestListener(req, res) {
-//   let forwarded = req.headers['x-forwarded-for'];
-//   let ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress;
-//   res.send(ip);
-// }
-// const server = http.createServer(requestListener);
-// server.listen(5000);
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // let forwarded = req.headers['x-forwarded-for'];
-  // let address = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress;
-  res.render('index', { title: 'Express' });
+  res.render('index', { title : 'Express' });
 });
 
-router.post('/addpin', jsonParser, function(req, res) {
+router.post('/addpin', function(req, res) {
   var db = req.db;
   var pins = db.collection('pins');
   pins.insert(req.body, function(e, result) {
     res.send(
-      (e === null) ? {msg : '', body: req.body} : {msg : "Add pin Error: " + e}
+      (e === null) ? { msg : '', body : req.body } : { msg : "Add pin Error: " + e }
     );
   });
 });
@@ -35,16 +22,16 @@ router.get('/toppin', function(req, res) {
   var pins = db.collection('pins');
   pins.findOne({}, { sort : { _id : -1 } }, function(e, docs) {
     res.json(docs);
-  })
-})
+  });
+});
 
 router.delete('/deletepin', function(req, res) {
   var db = req.db;
   var pins = db.collection('pins');
   pins.findOneAndDelete({}, { sort : { _id : -1 } }, function(e, docs) {
-    res.send((e === null) ? {msg : ''} : {msg : "Delete pin Error: " + e});
+    res.send((e === null) ? { msg : '' } : { msg : "Delete pin Error: " + e });
   });
-})
+});
 
 router.get('/allpins', function(req, res) {
   var db = req.db;
